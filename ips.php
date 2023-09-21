0773cdcdf30d8cfc269103bbdf98602fb52ed47c
anasrami12 <145429442+anasrami12@users.noreply.github.com>
2023-09-21 09:34:40 +0200

    Add files via upload

---
+++ ips.php
@@ -0,0 +1,16 @@
+<HTML>
+<HEAD><TITLE> EJ1-Conversion IP Decimal a Binario </TITLE></HEAD>
+<BODY>
+<?php
+$ip = "192.168.16.100/16"; 
+echo "La ip completa es ";
+$divIP = explode("/", $ip);//dividir
+echo "La mascara es: " $divIP[1];
+
+
+
+
+
+?>
+</BODY>
+</HTML>
