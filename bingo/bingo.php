b8b6cdb42993ff2e0e46b1032d348e09b9e6edd4
anasrami12 <145429442+anasrami12@users.noreply.github.com>
2023-10-24 09:51:26 +0200

    Update bingo.php

--- bingo/bingo.php
+++ bingo/bingo.php
@@ -16,18 +16,27 @@ function generarJugadoresYCarton(){
     //generamos los jugadores
     $jugadores = array('jugador1','','');
     //en cada jugador generamos los cartones
-    $jugadores['jugador1']= array('carton1','','');
+    $jugadores['jugador1']= array('carton1','carton2','carton3');
     //rellenamos cada carton de numeros aleatorios del 1-60
     $jugador1['carton1'] = array();
+    $jugador1['carton2'] = array();
+    $jugador1['carton3'] = array();
     //mientras el tamaño de carton1 sea menor a 15 se ejecutara el while
-    while (count($jugador1['carton1']) < 15) {
-        $random = rand(1,60);
-        // generamos el numero SOLO si no se encuentra en el array
-        if (!in_array($random, $jugador1['carton1'])) {
-            $jugador1['carton1'][] = $random;
+    $cartones = 3;
+for ($i = 1; $i <= $cartones; $i++) {
+    while (count($jugador1['carton' . $i]) < 15) {
+        $random = rand(1, 60);
+        if (!in_array($random, $jugador1['carton' . $i])) {
+            $jugador1['carton' . $i][] = $random;
         }
     }
+}
+
+
+
     return $jugador1['carton1'];
+    
+
 
 }
 
